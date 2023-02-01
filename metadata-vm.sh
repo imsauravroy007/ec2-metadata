2c818f743c68c44ee4ebeaf8cb0093b80dbf5ad8
Saurav Roy <rsaurav81@yahoo.com>
2023-02-01 12:39:59 +0530

    vm metadata challenge changes

--- metadata-vm.sh
+++ metadata-vm.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-res="test"
+
 
 if [ -z "$1" ]
 then
     #curl "http://metadata.google.internal/computeMetadata/v1/METADATA_KEY?wait_for_change=true" -H "Metadata-Flavor: Google"
-    #curl http://localhost:8880/test.json | python -mjson.tool > out.json
-    curl https://jsonplaceholder.typicode.com/todos/1
+    
+    curl http://metadata.google.internal/computeMetadata/v1/instance/$1?alt=json -H "Metadata-Flavor: Google"
 else
-    curl https://jsonplaceholder.typicode.com/todos/1
+    curl http://metadata.google.internal/computeMetadata/v1/instance/$1?alt=json -H "Metadata-Flavor: Google"
     
 fi
 
-echo $res
+
